5f4266a88438bfd5f4a5b602afbc1dd85e6f0e22
Annika Holmqvist <annika.holmqvist94@gmail.com>
2026-04-29 15:34:17 +0200

    Add `Message` record to model conversation messages with `Role` enum

---
+++ src/main/java/se/veldrift/devmentor/domain/Message.java
@@ -0,0 +1,23 @@
+package se.veldrift.devmentor.domain;
+
+/**
+ * Ett meddelande i en konversation.
+ */
+public record Message(Role role, String content) {
+
+    public enum Role {
+        SYSTEM, USER, ASSISTANT
+    }
+
+    public static Message system(String content) {
+        return new Message(Role.SYSTEM, content);
+    }
+
+    public static Message user(String content) {
+        return new Message(Role.USER, content);
+    }
+
+    public static Message assistant(String content) {
+        return new Message(Role.ASSISTANT, content);
+    }
+}
